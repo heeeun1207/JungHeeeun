05ae24a20c70810653e563f1dc899d26862fe653
jungheeeun <jungheeeun272@gmail.com>
2023-06-24 21:54:25 +0900

    react - ts

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Welcome to My Portfolio</h1>
+      <p>This is my personal website built with React and TypeScript.</p>
+    </div>
+  );
+}
+
+export default App;
