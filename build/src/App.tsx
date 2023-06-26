17bdcb30f90eb0889e8c0b9c64b029e4434f416c
jungheeeun <jungheeeun272@gmail.com>
2023-06-26 23:49:37 +0900

    Fragment 기능

--- build/src/App.tsx
+++ build/src/App.tsx
@@ -1,16 +1,22 @@
 import React from 'react';
 import UserFirstart from './components/UserFirestart';
 import Video from './components/Video';
+import { Fragment } from 'react';
 
 function App() {
   return (
+    
     <div>
       <UserFirstart />
       <Video />
       <div>
         Hello <b>react</b>
       </div>
+      <Fragment>
+        <h1>리액트 안녕 !</h1>
+      </Fragment>
     </div>
+    
   );
 }
 
