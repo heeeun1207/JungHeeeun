3972dda6c5f18897b23f0068b082cfbeb8ad9d59
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 00:02:53 +0900

    self-closing 태그

--- build/src/App.tsx
+++ build/src/App.tsx
@@ -1,20 +1,21 @@
 import React from 'react';
 import UserFirstart from './components/UserFirestart';
 import Video from './components/Video';
+import Nemo from './components/Nemo';
 import { Fragment } from 'react';
 
 function App() {
+  const name ='리액트';
   return (
     
     <div>
+      <>
+      <div className='react'>{name}</div>
+      <input />
+      </>
       <UserFirstart />
       <Video />
-      <div>
-        Hello <b>react</b>
-      </div>
-      <>
-        <h1>리액트 안녕 !</h1>
-      </>
+      <Nemo />
     </div>
     
   );
---
+++ build/src/components/Nemo.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './Nemo.css';
+
+function Nemo() {
+  return <div className="Nemo-container"></div>;
+}
+
+export default Nemo;
