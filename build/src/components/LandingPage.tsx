8b9a88544b8c944d431a813107e8b5b70943008d
jungheeeun <jungheeeun272@gmail.com>
2023-06-27 01:50:19 +0900

    깃허브 링크 , 아이콘 추가 - react-icons

--- build/src/components/LandingPage.tsx
+++ build/src/components/LandingPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiGithub } from 'react-icons/fi';
 import './LandingPage.css';
 
 const LandingPage = () => {
@@ -6,6 +7,13 @@ const LandingPage = () => {
     <div className="landing-page">
       <div className="content">
         <h1 className="title">Jungheeun 포트폴리오</h1>
+        <a
+          href="https://github.com/heeeun1207"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FiGithub size={20} /> GitHub 링크
+        </a>
       </div>
     </div>
   );
